6022128e409f067cbbbdcd7f4b6761077e1449e6
NicoleValentina <nivacea@gmail.com>
2022-01-24 23:41:49 -0300

    Desafio Simulador interactivo

--- js/main.js
+++ js/main.js
@@ -1,12 +1,53 @@
-let pago = prompt("Ingresa tu pago de $25.000")
+let precio = 0;
+let productos = parseInt(
+  prompt(
+    "Selecciona un producto \n1. Polerón \n2. Pantalón \n3. Beanie \n4. Banano", "Ingresa una opción del 1 al 4"
+  )
+);
 
-while (pago < 25000) {
-    alert("Monto pagado: $" + pago + ", te faltan: $" + (25000 - pago) + " 😥")
-    pago = prompt("Ingresa tu pago de $25.000")
+switch (productos) {
+  case 1:
+    precio = 20000;
+    break;
+
+  case 2:
+    precio = 25000;
+    break;
+
+  case 3:
+    precio = 10000;
+    break;
+
+  case 4:
+    precio = 15000;
+    break;
+
+  default:
+    precio = 0;
+    break;
 }
 
-if(pago == 25000){
-    alert("¡Muchas gracias por tu compra! 🙌")
-} else if (pago > 25000){
-    alert ("Tu vuelto es: $" + (pago - 25000) + ", ¡muchas gracias por tu compra! 🙌")
+if (precio == 0) {
+  alert("¿Estás vitrineando? Vuelve pronto, te esperamos ✌️");
+
+} else if (precio > 0) {
+
+  let pago = prompt("Ingresa tu pago de $" + precio);
+
+  while (pago < precio) {
+    alert(
+      "Monto pagado: $" + pago + ", te faltan: $" + (precio - pago) + " 😥"
+    );
+    pago = prompt("Ingresa tu pago de $" + precio);
+  }
+
+  if (pago == precio) {
+    alert("¡Muchas gracias por tu compra! 🙌");
+  } else if (pago > precio) {
+    alert(
+      "Tu vuelto es: $" +
+        (pago - precio) +
+        ", ¡muchas gracias por tu compra! 🙌"
+    );
+  }
 }
