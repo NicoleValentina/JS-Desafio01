305f7f447f15ba001982fb157d34887afc4a3d56
NicoleValentina <nivacea@gmail.com>
2022-02-08 16:16:44 -0300

    Desafío Complementario 3

--- js/main.js
+++ js/main.js
@@ -1,116 +1,34 @@
 const inventario = [
-  { nombre: "Polerón", precio: 20000, stock: 10 },
-  { nombre: "Pantalón", precio: 25000, stock: 15 },
-  { nombre: "Beanie", precio: 10000, stock: 8 },
-  { nombre: "Banano", precio: 15000, stock: 10 },
+  { nombre: "Polerón", precio: 20000, stock: 10, imagen: "media/poleron.jpg"},
+  { nombre: "Pantalón", precio: 25000, stock: 15, imagen: "media/pantalon.jpg"},
+  { nombre: "Beanie", precio: 10000, stock: 8, imagen: "media/beanie.jpg" },
+  { nombre: "Banano", precio: 15000, stock: 10, imagen: "media/banano.jpg" },
 ];
 
-//Mostrar detalle de cada producto (por consola)
-inventario.forEach((prod) => {
-  console.log(prod);
-})
 
+let contenedor = document.getElementById("contenedor")
 
-//Selección de productos
-let productos = parseInt(
-  prompt(
-    "Selecciona un producto \n1. Polerón \n2. Pantalón \n3. Beanie \n4. Banano",
-    "Ingresa una opción del 1 al 4"
-  )
-);
-
-let precio = 0, nombre = 0, stock = 0, cantidad = 0
-
-switch (productos) {
-  case 1:
-    nombre = inventario[0].nombre;
-    precio = inventario[0].precio;
-    stock = inventario[0].stock;
-    cantidad =  prompt(
-      "Ingresa la cantidad de " + nombre + " que deseas" +
-        "\nHay " + stock + " disponibles"
-    )
-    break;
-
-  case 2:
-    nombre = inventario[1].nombre;
-    precio = inventario[1].precio;
-    stock = inventario[1].stock;
-    cantidad =  prompt(
-      "Ingresa la cantidad de " + nombre + " que deseas" +
-        "\nHay " + stock + " disponibles")
-    break;
-
-  case 3:
-    nombre = inventario[2].nombre;
-    precio = inventario[2].precio;
-    stock = inventario[2].stock;
-    cantidad =  prompt(
-      "Ingresa la cantidad de " + nombre + " que deseas" +
-        "\nHay " + stock + " disponibles")
-    break;
-
-  case 4:
-    nombre = inventario[3].nombre;
-    precio = inventario[3].precio;
-    stock = inventario[3].stock;
-    cantidad =  prompt(
-      "Ingresa la cantidad de " + nombre + " que deseas" +
-        "\nHay " + stock + " disponibles")
-    break;
-
-  default:
-    precio = 0;
-    break;
+//Creación de los divs para meter la info de producto
+for (let index = 0; index < inventario.length; index++) {
+  let contenedorProducto = document.createElement("div")
+  contenedor.appendChild(contenedorProducto)
+  contenedorProducto.className = "colProd"
 }
 
 
-//Función para realizar pago
-function checkout() {
-  let carrito = [nombre, precio, cantidad],
-    total = parseInt(carrito[1] * carrito[2]);
+let producto = document.getElementsByClassName("colProd")
 
-  let pago = prompt(
-    "Has seleccionado " + carrito[2] + " x " + carrito[0] + " a " + carrito[1] + " c/u" +
-    "\nIngresa tu pago de $" + total
-  );
+//Función para llamar y estructurar los datos de los productos
+function datosProducto(i) {
+  producto[i].innerHTML = `
+  <img class="producto" src="${inventario[i].imagen}" alt=${inventario[i].nombre}>
+  <h3>${inventario[i].nombre}</h3>
+  <h4>$${inventario[i].precio}</h4>
+  <p>Hay ${inventario[i].stock} en stock</p>
+  <button>Agregar al carro</button>`
 
-  carrito.push(pago);
-
-  //Si el pago no está completo
-  while (pago < total) {
-    alert("Monto pagado: $" + pago + ", te faltan: $" + (total - pago) + " 😥");
-
-    pago = prompt(
-      "Has seleccionado " + carrito[2] + " x " + carrito[0] + " a " + carrito[1] + " c/u" +
-      "\nIngresa tu pago de $" + total
-    );
-  }
-  carrito.pop();
-  carrito.push(pago);
-
-  let vuelto = parseInt(carrito[3] - total);
-
-  //Pago completo
-  if (pago == total) {
-    alert(
-      "Comppraste " + carrito[2] + " x " + carrito[0] + ", a $" + carrito[1] + " c/u " +
-        "\n¡Muchas gracias por tu compra! 🙌"
-    );
-  } else if (pago > total) {
-    alert(
-      "Tu vuelto es: $" + vuelto +
-      "\nCompraste " + carrito[2] + " x " + carrito[0] + ", a $" + carrito[1] + " c/u " +
-        "\n¡Muchas gracias por tu compra! 🙌"
-    );
-  }
-}
-//Fin de la función
-
-//Trigger de pago
-if (precio == 0) {
-  alert("¿Estás vitrineando? Vuelve pronto, te esperamos ✌️");
-} else if (precio > 0) {
-  checkout();
 }
 
+for (let index = 0; index < inventario.length; index++) {
+  datosProducto(index) 
+}
